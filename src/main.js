6ccf1ce07979665bd187eba6d9589b6d1baf162c
mithridatem <mathieumithridate@adrar-formation.com>
2025-09-25 09:44:16 +0200

    feat : exemple d'utilisation de fetch

--- src/main.js
+++ src/main.js
@@ -4,7 +4,32 @@ import { apiKey } from './env.js'
 //Fetch (requête) sur l'API météo
 const meteo = fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=' + apiKey)
   .then(response=>response.json())
-  .then(data=>{ 
+  .then(data =>{ 
     console.log(data)
   });
 
+//Version avec fonction asynchrone et await
+const apiMeteo = async () =>{
+	return await fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=' + apiKey)
+		.then(response => {
+			return response.json();
+		});
+}
+
+//Appel de la méthode (traitement)
+apiMeteo().then(json => {
+	console.log(json);
+});
+
+//Version alternative avec une fonction nommée
+async function apiFunction() {
+	return await fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=' + apiKey)
+		.then(response => {
+			return response.json();
+		});
+}
+
+//Appel de la méthode (traitement)
+apiFunction().then(json => {
+	console.log(json);
+})
